aa8904573fce44882379703fa2b2715a5cdc91c0
Ronny Mandal <ronnyma@transfinite.no>
2018-01-09 21:10:37 +0100

    Initial

---
+++ src/main/java/no/transfinite/be/MockServer.java
@@ -0,0 +1,88 @@
+package no.transfinite.be;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.client.WireMock;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
+import static com.github.tomakehurst.wiremock.stubbing.Scenario.STARTED;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class MockServer
+{
+    public static void main( String[] args )
+    {
+        WireMockServer wireMockServer = new WireMockServer(wireMockConfig().port(8080)); //No-args constructor will start on port 8080, no HTTPS
+
+        wireMockServer.start();
+
+        stubFor(get(urlEqualTo("/name"))
+                .willReturn(aResponse()
+                        .withHeader("Content-Type", "text/plain")
+                        .withBody("Greker Greker")));
+
+        stubFor(get(urlEqualTo("/id/Per"))
+                .willReturn(aResponse()
+                        .withHeader("Content-Type", "application/json")
+                        .withBody("{\"name\" : \"Per\", \"id\" : 1337}")));
+
+        stubFor(get(urlEqualTo("/id/Paal"))
+                .willReturn(aResponse()
+                        .withHeader("Content-Type", "application/json")
+                        .withBody("{\"name\" : \"Per\", \"id\" : 4489}")));
+
+        stubFor(delete(urlEqualTo("/id/Per"))
+                .willReturn(unauthorized()));
+
+        stubFor(get(urlEqualTo("/private_pictures"))
+                .willReturn(aResponse()
+                        .withStatus(403)));
+
+        stubFor(get(urlEqualTo("/smart/Trump"))
+                .willReturn(aResponse()
+                        .withStatus(404)));
+
+        stubFor(get(urlEqualTo("/forever"))
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withFixedDelay(9000)));
+
+        stubFor(get(urlEqualTo("/salary/Per")).inScenario("UpSalary")
+                .whenScenarioStateIs(STARTED)
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withHeader("Content-Type", "application/json")
+                        .withBody("{\"name\" : \"Per\", \"id\" : 4489, \"salary\" : 300000}")));
+
+
+        stubFor(put(urlEqualTo("/salary/Per")).inScenario("UpSalary")
+                .whenScenarioStateIs(STARTED)
+                .willReturn(aResponse()
+                        .withHeader("Content-Type", "application/json")
+                        .withBody("{\"msg\" : \"Salary up for audit by supervisor.\"}"))
+                .willSetStateTo("B"));
+
+        stubFor(get(urlEqualTo("/salary/Per")).inScenario("UpSalary")
+                .whenScenarioStateIs("B")
+                .willReturn(aResponse()
+                        .withHeader("Content-Type", "application/json")
+                        .withBody("{\"msg\" : \"Audit not yet completed.\"}"))
+                .willSetStateTo("C"));
+
+        stubFor(get(urlEqualTo("/salary/Per")).inScenario("UpSalary")
+                .whenScenarioStateIs("C")
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withHeader("Content-Type", "application/json")
+                        .withBody("{\"name\" : \"Per\", \"id\" : 4489, \"salary\" : 900000}")));
+
+    }
+}
